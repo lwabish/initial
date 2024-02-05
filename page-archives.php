15a4fc3e762a91a135adc40cc0380274ca06fde8
lwabish <wubw@pku.edu.cn>
2024-02-05 16:33:02 +0800

    perf: line hight

--- page-archives.php
+++ page-archives.php
@@ -23,9 +23,9 @@ while($archives->next()){
 		$output .= '<h3>'.date('Y 年',$archives->created).'</h3><ul>';
 	}
 	if ($this->options->PjaxOption && $archives->hidden) {
-		$output .= '<li>'.date('m/d：',$archives->created).'<a>'. $archives->title .'</a></li>';
+		$output .= '<li style="line-height: 2.0;">'.date('m/d：',$archives->created).'<a>'. $archives->title .'</a></li>';
 	} else {
-		$output .= '<li>'.date('m/d：',$archives->created).'<a href="'.$archives->permalink .'">'. $archives->title .'</a></li>';
+		$output .= '<li style="line-height: 2.0;">'.date('m/d：',$archives->created).'<a href="'.$archives->permalink .'">'. $archives->title .'</a></li>';
 	}
 }
 $output .= '</ul></div>';
